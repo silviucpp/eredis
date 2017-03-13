6e27a379e69ab0c3d06ad70cc9e6780f385ae3aa
Silviu Caragea <silviu.caragea@woowteam.com>
2017-03-13 16:42:26 +0200

    Add keepalive

--- include/eredis.hrl
+++ include/eredis.hrl
@@ -33,7 +33,7 @@
 
 -define(NL, "\r\n").
 
--define(SOCKET_OPTS, [binary, {active, once}, {packet, raw}, {reuseaddr, true},
+-define(SOCKET_OPTS, [binary, {active, once}, {packet, raw}, {reuseaddr, true}, {keepalive, true},
                       {send_timeout, ?SEND_TIMEOUT}, {send_timeout_close, true}]).
 
 -define(RECV_TIMEOUT, 5000).
